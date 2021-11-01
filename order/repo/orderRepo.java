6f884050a782009e7f58ad89174d4954bffab899
Manoj-12345 <89392983+Manoj-12345@users.noreply.github.com>
2021-11-01 23:07:12 +0530

    Add files via upload
    orderservice

---
+++ order/repo/orderRepo.java
@@ -0,0 +1,12 @@
+package com.order.repo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.order.details.orderDetails;
+
+
+
+public interface orderRepo extends  MongoRepository<orderDetails, String> {
+
+}
+
